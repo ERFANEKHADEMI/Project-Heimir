de2ecdb1699747920463a0374fbca1f56cf77bf6
lxRbckl <lxrbckl@gmail.com>
2024-06-13 00:55:36 -0500

    new fs

---
+++ app.ts
@@ -0,0 +1,4 @@
+// < Project Heimir by Alex Arbuckle > //
+
+
+//
---
+++ src/services/archiveManager.ts
@@ -0,0 +1,49 @@
+// imports <
+import {} from 'lxrbckl';
+
+// >
+
+
+class archiveManager {
+
+
+   private _githubUsersLink: string;
+   private _octokitConfigLink: string;
+
+
+   constructor(
+
+      githubUsersLink: string,
+      octokitConfigLink: string
+
+   ) {
+
+      this._githubUsersLink = githubUsersLink;
+      this._octokitConfigLink = octokitConfigLink;
+      
+   }
+
+
+   getReposFromUsers() {
+
+
+
+   }
+
+
+   getStackFromRepo() {
+
+
+
+   }
+
+}
+
+
+
+
+(async () => {
+
+
+
+})();
